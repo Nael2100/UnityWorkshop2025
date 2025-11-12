bfd66e21d83a41a28de14b36a858c2b32da52714
OptimistGD <optimist.professionel@gmail.com>
2025-11-12 11:11:36 +0100

    Skills personnages v.1

---
+++ Assets/Scripts/Gameplay/TowerDefenseGameplay/Skills/CalamitySkills/StrongblowSkill.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace TBT.Gameplay
+{
+    public class StrongblowSkill : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
